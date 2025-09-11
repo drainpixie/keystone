1036ba7ee570b448c80a3e72f70fa110b7a8df01
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-09-11 16:00:02 +0200

    fix: stop smoking

--- systems/incubator/disko.nix
+++ systems/incubator/disko.nix
@@ -1,30 +1,41 @@
 {
-  disko.devices = {
-    disk.vda = {
-      type = "disk";
-      device = "/dev/vda";
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/vda";
 
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = {
-          ESP = {
-            size = "512MiB";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "512M";
+          type = "EF00";
+          device = "/dev/disk/by-label/NIXBOOT";
+          content = {
+            format = "vfat";
+            type = "filesystem";
+            mountpoint = "/boot";
+            mountOptions = ["defaults"];
+
+            extraArgs = [
+              "-n"
+              "NIXBOOT"
+            ];
           };
+        };
 
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-            };
+        root = {
+          size = "100%";
+          device = "/dev/disk/by-label/NIXROOT";
+          content = {
+            format = "ext4";
+            mountpoint = "/";
+            type = "filesystem";
+            mountOptions = ["defaults"];
+
+            extraArgs = [
+              "-L"
+              "NIXROOT"
+            ];
           };
         };
       };
